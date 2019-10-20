5d532017ac10c8f059a22997523499adb3c7f842
Jing Xiang <jxsim.2013@gmail.com>
2019-10-20 16:29:39 +0800

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-project-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-project-manager');
+    expect(app.title).toEqual('project-manager-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-project-manager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('project-manager-angular app is running!');
   });
 });
